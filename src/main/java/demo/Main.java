cf63793a52e0c8571efc132b1cd98c277e2df635
Gelin Luo <greenlaw110@gmail.com>
2017-07-15 08:41:43 +1000

    add comment to Main controller methods; simplified create handler

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -19,32 +19,108 @@ import static act.controller.Controller.Util.notFoundIfNull;
 @UrlContext("/accounts")
 public class Main {
 
+    /**
+     * Fetch a list of accounts
+     *
+     * HTTP request sample:
+     *
+     * ```
+     * GET /accounts      # in normal html response, template generate response
+     * GET /accounts/json # in JSON response, framework generate response
+     * ```
+     */
     @GetAction
     public List<Account> list() {
         return Account.dao.find("select * from account");
     }
 
+
+    /**
+     * Fetch an account by id
+     *
+     * HTTP request sample:
+     *
+     * ```
+     * GET /accounts/123         # request an account with ID = 123
+     * GET /accounts?id=123      # request an account with ID = 123
+     * GET /accounts/json?id=123 # request an account with ID = 123 in JSON response
+     * GET /accounts/123/json    # request an account with ID = 123 in JSON response
+     * ```
+     */
     @GetAction("{id}")
     public Account fetch(String id) {
         return Account.dao.findById(id);
     }
 
+    /**
+     * Create an new account
+     *
+     * HTTP request sample
+     *
+     * ```
+     * POST /accounts
+     * ```
+     *
+     * The post body should be different when `Content-Type` is different:
+     *
+     * # Content-Type = application/x-www-form-urlencoded
+     * ```
+     * account.name=张三
+     * account.password=li-si
+     * ```
+     *
+     * # Content-Type = application/json
+
+     * ```
+     * {
+     *     "name": "张三"
+     *     "password: "li-si"
+     * }
+     * ```
+     *
+     * or
+     *
+     * ```
+     * {
+     *      "account" : {"name": "张三", "password: "li-si"}
+     * }
+     * ```
+     *
+     */
     @PostAction
     @With(JFinalTransactional.class)
-    public Account create(String name, String password) {
-        Account newAccount = new Account();
-        newAccount.setName(name);
-        newAccount.setPassword(password);
-        newAccount.save();
-        return newAccount;
+    public Account create(Account account) {
+        account.save();
+        return account;
     }
 
+    /**
+     * Delete an account by ID
+     *
+     * HTTP Request sample
+     *
+     * ```
+     * DELETE /accounts/123
+     * DELETE /accounts?id=123
+     * ```
+     */
     @DeleteAction("{id}")
     @With(JFinalTransactional.class)
     public void delete(String id) {
         Account.dao.deleteById(id);
     }
 
+    /**
+     * Update an account field value
+     *
+     * HTTP Request sample
+     *
+     * ```
+     * PUT /accounts/123/name?value=李四
+     * PUT /accounts/123/password?value=zhang-san
+     * PUT /accounts/123/age?value=18&type=int
+     * ```
+     */
     @PutAction("{id}/{field}")
     @With(JFinalTransactional.class)
     public void update(String id, String field, String value, @DefaultValue("String.class") String type, StringValueResolverManager resolver) {
